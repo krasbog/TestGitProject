51278f056ee10968a903e9806e9a69f043016c6b
Николай Медведев <krasbog@mail.ru>
2023-12-04 21:24:46 +0300

    Последнее изм

--- ConsoleAppGitTest/Program.cs
+++ ConsoleAppGitTest/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             int a = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{a}*{a}={Logic.PowerOf2(a)}");
+            Console.WriteLine($"{a}*{a}={Logic.PowerOf2(a,2)}");
             
         }
     }
