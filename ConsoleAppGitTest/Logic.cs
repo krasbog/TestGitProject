0b0fe8570637b19033c8731148fb3e2549ae4b68
Николай Медведев <krasbog@mail.ru>
2023-12-04 20:01:09 +0300

    Добавили файл с логикой

---
+++ ConsoleAppGitTest/Logic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitTest
+{
+    internal class Logic
+    {
+        public int PowerOf2 (int a) { return a*a; }
+    }
+}
